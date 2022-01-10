9e85311eac22d0296251b1857525746de501fa25
Florent Kermarrec <florent@enjoy-digital.fr>
2022-01-10 08:54:26 +0100

    lms7002m: Initial TX/RX clocking split (but still use same clock source: mclk1).

--- gateware/lms7002m.py
+++ gateware/lms7002m.py
@@ -77,27 +77,32 @@ class LMS7002M(Module, AutoCSR):
             spi_clk_freq = 1e6
         )
 
-        # Clk-Measurement.
+        # Clocking.
         # ----------------
-        self.clock_domains.cd_rfic = ClockDomain("rfic")
-        self.comb += self.cd_rfic.clk.eq(pads.mclk1)
+        self.clock_domains.cd_rfic_rx = ClockDomain("rfic_rx")
+        self.clock_domains.cd_rfic_tx = ClockDomain("rfic_tx")
 
+        # RX & Measurement.
+        self.comb += self.cd_rfic_rx.clk.eq(pads.mclk1)
         cycles = Signal(32)
-        self.sync.rfic += cycles.eq(cycles + 1)
+        self.sync.rfic_rx += cycles.eq(cycles + 1)
         self.sync += If(self.cycles_latch.re, self.cycles.status.eq(cycles))
+
+        # TX.
+        self.comb += self.cd_rfic_tx.clk.eq(pads.mclk1)
 
         # TX Datapath.
         # ------------
 
         # FIXME: Add proper Clk Phase support/calibration without ODELAYE2.
-        self.submodules.tx_cdc  = tx_cdc  = stream.ClockDomainCrossing([("data", 64)], cd_from="sys", cd_to="rfic")
-        self.submodules.tx_conv = tx_conv = ClockDomainsRenamer("rfic")(stream.Converter(64, 32))
+        self.submodules.tx_cdc  = tx_cdc  = stream.ClockDomainCrossing([("data", 64)], cd_from="sys", cd_to="rfic_tx")
+        self.submodules.tx_conv = tx_conv = ClockDomainsRenamer("rfic_tx")(stream.Converter(64, 32))
         self.comb += self.sink.connect(tx_cdc.sink)
         self.comb += tx_cdc.source.connect(tx_conv.sink)
 
         # TX Pattern (Counter).
         tx_pattern = Signal(32)
-        self.sync.rfic += tx_pattern.eq(tx_pattern + 1)
+        self.sync.rfic_tx += tx_pattern.eq(tx_pattern + 1)
 
         # Pattern/Data Mux.
         self.comb += [
@@ -112,7 +117,7 @@ class LMS7002M(Module, AutoCSR):
         # TX Clk.
         self.specials += Instance("ODDR",
             p_DDR_CLK_EDGE = "SAME_EDGE",
-            i_C  = ClockSignal("rfic"),
+            i_C  = ClockSignal("rfic_tx"),
             i_CE = 1,
             i_S  = 0,
             i_R  = 0,
@@ -122,10 +127,10 @@ class LMS7002M(Module, AutoCSR):
         )
 
         # TX Frame.
-        self.sync.rfic += tx_frame.eq(~tx_frame)
+        self.sync.rfic_tx += tx_frame.eq(~tx_frame)
         self.specials += Instance("ODDR",
             p_DDR_CLK_EDGE = "SAME_EDGE",
-            i_C  = ClockSignal("rfic"),
+            i_C  = ClockSignal("rfic_tx"),
             i_CE = 1,
             i_S  = 0,
             i_R  = 0,
@@ -138,7 +143,7 @@ class LMS7002M(Module, AutoCSR):
         for n in range(12):
             self.specials += Instance("ODDR",
                 p_DDR_CLK_EDGE = "SAME_EDGE",
-                i_C  = ClockSignal("rfic"),
+                i_C  = ClockSignal("rfic_tx"),
                 i_CE = 1,
                 i_S  = 0,
                 i_R  = 0,
@@ -150,8 +155,8 @@ class LMS7002M(Module, AutoCSR):
         # RX Datapath.
         # ------------
         # FIXME: Add proper Clk Phase support/calibration.
-        self.submodules.rx_cdc  = rx_cdc  = stream.ClockDomainCrossing([("data", 64)], cd_from="rfic", cd_to="sys")
-        self.submodules.rx_conv = rx_conv = ClockDomainsRenamer("rfic")(stream.Converter(32, 64))
+        self.submodules.rx_cdc  = rx_cdc  = stream.ClockDomainCrossing([("data", 64)], cd_from="rfic_rx", cd_to="sys")
+        self.submodules.rx_conv = rx_conv = ClockDomainsRenamer("rfic_rx")(stream.Converter(32, 64))
         self.comb += rx_conv.source.connect(rx_cdc.sink)
         self.comb += rx_cdc.source.connect(self.source)
 
@@ -168,7 +173,7 @@ class LMS7002M(Module, AutoCSR):
         # RX Frame.
         self.specials += Instance("IDDR",
             p_DDR_CLK_EDGE = "SAME_EDGE_PIPELINED",
-            i_C  = ClockSignal("rfic"),
+            i_C  = ClockSignal("rfic_rx"),
             i_CE = 1,
             i_S  = 0,
             i_R  = 0,
@@ -181,7 +186,7 @@ class LMS7002M(Module, AutoCSR):
         for n in range(12):
             self.specials += Instance("IDDR",
                 p_DDR_CLK_EDGE = "SAME_EDGE_PIPELINED",
-                i_C  = ClockSignal("rfic"),
+                i_C  = ClockSignal("rfic_rx"),
                 i_CE = 1,
                 i_S  = 0,
                 i_R  = 0,
