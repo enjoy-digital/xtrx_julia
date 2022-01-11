d63dc02e34b256cf49898591aac1059530b3481f
Florent Kermarrec <florent@enjoy-digital.fr>
2022-01-11 14:36:47 +0100

    test_lms7002m_calib: ADD RX delay scan and TX/RX delay set commands.

--- test_lms7002m_calib.py
+++ test_lms7002m_calib.py
@@ -6,33 +6,18 @@ import argparse
 
 from litex import RemoteClient
 
-# TX Clk Delay test --------------------------------------------------------------------------------
 
-def lms7002m_tx_clk_delay_test(port, delay):
-    bus = RemoteClient(port=port)
-    bus.open()
-
-    assert delay < 32
-
-    # Reset Delay.
-    bus.regs.lms7002m_tx_clk_rst.write(1)
-
-    # Configure Delay.
-    for i in range(delay):
-        bus.regs.lms7002m_tx_clk_inc.write(1)
-
-    bus.close()
-
-
-# LMS7002M Delay calib test ------------------------------------------------------------------------
+# LMS7002M Delay Scan ------------------------------------------------------------------------------
 
 PATTERN_ENABLE     =   (1 << 0)
 PATTERN_COUNT_MODE = 0*(1 << 1)
 PATTERN_PRBS_MODE  = 1*(1 << 1)
 
-def lms7002m_delay_tx_calib_test(port):
+def lms7002m_delay_tx_scan(port):
     bus = RemoteClient(port=port)
     bus.open()
+
+    print("TX Delay Scan...")
 
     # Reset Delay.
     bus.regs.lms7002m_tx_clk_rst.write(1)
@@ -58,9 +43,11 @@ def lms7002m_delay_tx_calib_test(port):
 
     bus.close()
 
-def lms7002m_delay_rx_calib_test(port):
+def lms7002m_delay_rx_scan(port):
     bus = RemoteClient(port=port)
     bus.open()
+
+    print("RX Delay Scan...")
 
     # Reset Delay.
     bus.regs.lms7002m_rx_delay_rst.write(1)
@@ -86,27 +73,63 @@ def lms7002m_delay_rx_calib_test(port):
 
     bus.close()
 
+# LMS7002M Delay Set -------------------------------------------------------------------------------
+
+def lms7002m_delay_tx_set(port, delay):
+    bus = RemoteClient(port=port)
+    bus.open()
+
+    assert delay < 32
+
+    # Reset Delay.
+    bus.regs.lms7002m_tx_clk_rst.write(1)
+
+    # Configure Delay.
+    for i in range(delay):
+        bus.regs.lms7002m_tx_clk_inc.write(1)
+
+    bus.close()
+
+def lms7002m_delay_rx_set(port, delay):
+    bus = RemoteClient(port=port)
+    bus.open()
+
+    assert delay < 32
+
+    # Reset Delay.
+    bus.regs.lms7002m_rx_delay_rst.write(1)
+
+    # Configure Delay.
+    for i in range(delay):
+        bus.regs.lms7002m_rx_delay_inc.write(1)
+
+    bus.close()
+
 # Run ----------------------------------------------------------------------------------------------
 
 def main():
     parser = argparse.ArgumentParser(description="TX Clk Delay utility")
-    parser.add_argument("--port",         default="1234",  help="Host bind port.")
-    parser.add_argument("--tx-clk-delay", default=None,     help="TX Clk Delay.")
-    parser.add_argument("--tx-calib",     action="store_true", help="Run TX calibration test.")
-    parser.add_argument("--rx-calib",     action="store_true", help="Run RX calibration test.")
+    parser.add_argument("--port",     default="1234",  help="Host bind port.")
+    parser.add_argument("--tx-scan",  action="store_true", help="Run TX delay scan.")
+    parser.add_argument("--rx-scan",  action="store_true", help="Run RX delay scan.")
+    parser.add_argument("--tx-delay", default=None,        help="Set TX delay.")
+    parser.add_argument("--rx-delay", default=None,        help="Set RX delay.")
 
     args = parser.parse_args()
 
     port = int(args.port, 0)
 
-    if args.tx_clk_delay is not None:
-        lms7002m_tx_clk_delay_test(port=port, delay=int(args.tx_clk_delay, 0))
+    if args.tx_scan:
+        lms7002m_delay_tx_scan(port=port)
 
-    if args.tx_calib:
-        lms7002m_delay_tx_calib_test(port=port)
+    if args.rx_scan:
+        lms7002m_delay_rx_scan(port=port)
 
-    if args.rx_calib:
-        lms7002m_delay_rx_calib_test(port=port)
+    if args.tx_delay is not None:
+        lms7002m_delay_tx_set(port=port, delay=int(args.tx_delay, 0))
+
+    if args.rx_delay is not None:
+        lms7002m_delay_rx_set(port=port, delay=int(args.rx_delay, 0))
 
 if __name__ == "__main__":
     main()
