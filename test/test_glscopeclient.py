1acf4a489bbb40a44f2091454cf0013365b1319e
Florent Kermarrec <florent@enjoy-digital.fr>
2022-01-25 09:43:29 +0100

    test/test_glscopeclient: Adapt script to EnjoyDigitalGenericOscilloscope.

--- test/test_glscopeclient.py
+++ test/test_glscopeclient.py
@@ -6,7 +6,7 @@
 # Copyright (c) 2021-2022 Florent Kermarrec <florent@enjoy-digital.fr>
 # SPDX-License-Identifier: BSD-2-Clause
 
-# SCPI Server proof of concept, tested with ./glscopeclient --debug myscope:aklabs:lan:127.0.0.1
+# SCPI Server proof of concept, tested with ./glscopeclient --debug myscope:enjoy-digital:lan:127.0.0.1
 
 import os
 import sys
@@ -19,8 +19,14 @@ import threading
 # SCPI Server --------------------------------------------------------------------------------------
 
 class SCPIServer:
-    def __init__(self, bind_ip="localhost", control_port=5025, control_only=False, waveform_port=50101):
+    def __init__(self, bind_ip="localhost", channels=2, sample_rate=30.72e6, sample_depth=16384,
+        control_port  = 5025,
+        control_only  = False,
+        waveform_port = 50101):
         self.bind_ip       = bind_ip
+        self.channels      = channels
+        self.sample_rate   = sample_rate
+        self.sample_depth  = sample_depth
         self.control_port  = control_port
         self.control_only  = control_only
         self.waveform_port = waveform_port
@@ -53,14 +59,15 @@ class SCPIServer:
                     data = client.recv(1024).decode("UTF-8")
                     #print(data)
                     if "IDN?" in data:
-                        client.send(bytes("Julia/Enjoy-Digital,XTRX alternative Gateware/Firmware,0001,0.1\n", "UTF-8"))
-                        continue
+                        client.send(bytes(f"Enjoy-Digital,GEN00{self.channels},0001,0.1\n", "UTF-8"))
                     if "GAIN?" in data:
-                        client.send(bytes("1\n", "UTF-8"))
-                        continue
+                        client.send(bytes("0\n", "UTF-8"))
                     if "OFFS?" in data:
                         client.send(bytes("0\n", "UTF-8"))
-                        continue
+                    if "SAMP:TIM?" in data:
+                        client.send(bytes(f"{int(1e15/self.sample_rate):d}\n", "UTF-8"))
+                    if "SAMP:DEPTH?" in data:
+                        client.send(bytes(f"{int(self.sample_depth):d}\n", "UTF-8"))
 
             finally:
                 print("Control: Disconnect")
@@ -71,16 +78,44 @@ class SCPIServer:
             client, addr = self.waveform_sock.accept()
             print(f"Waveform: Connected with {addr[0]}:{str(addr[1])}")
             try:
+                n = 0
                 while True:
                     if pattern:
-                        periods = 1024
-                        ength   = 16384
-                        shift   = 1
-                        client.send(bytes([int(128+128*math.sin(periods*i*2*3.1415/length)) for i in range(length-shift)]))
+                        periods = 16
+                        length  = int(self.sample_depth/self.channels)
+                        shift   = 1024
+                        data    = [int(128+128*math.sin((n*shift + periods*i*2*3.1415)/length)) for i in range(length)]
+                        #print(len(data))
+                        client.send(bytes(data))
+                        n += 1
                     else:
-                        os.system("../software/user/litepcie_test record waveform.bin 16384")
+                        # FIXME: Proof of concept; do it differently and move sample processing/remapping to GLScopeClient?
+                        os.system(f"../software/user/litepcie_test record waveform.bin {int(self.sample_depth*4):d}")
                         with open("waveform.bin", "rb") as f:
-                            client.send(f.read())
+                            data = list(f.read())
+                            #print(f"{data[0]:02x} {data[1]:02x} {data[2]:02x} {data[3]:02x}")
+                            samples = []
+                            for n in range(self.sample_depth):
+                                assert self.channels in [1, 2]
+                                for c in range(self.channels):
+                                    # 12-bit to 8-bit.
+                                    if self.channels == 1:
+                                        sample = (data[4*n + 1] << 8) + data[4*n + 0]
+                                    if self.channels == 2:
+                                        sample = (data[2*n + 2*c + 1] << 8) + data[2*n + 2*c + 0]
+                                    sample = sample >> 8
+                                    sample = sample & 0xff
+                                    # 2's complement to decimal.
+                                    if sample & 0x80:
+                                        sample &= 0x7f
+                                        sample = 128 - sample
+                                    # Offset/Clamp.
+                                    sample = (sample + 128)
+                                    sample = min(sample, 255)
+                                    sample = max(sample,   0)
+                                    samples.append(sample)
+                            samples = bytes(samples)
+                            client.send(samples)
                         time.sleep(0.1)
             finally:
                 print("Waveform: Disconnect")
@@ -101,6 +136,9 @@ class SCPIServer:
 def main():
     parser = argparse.ArgumentParser(description="SCPI Server test.")
     parser.add_argument("--bind-ip",       default="localhost", help="Host bind address.")
+    parser.add_argument("--channels",      default=1,           help="Number of Channels.")
+    parser.add_argument("--sample-rate",   default=30.72e6,     help="Samplerate (per channel).")
+    parser.add_argument("--sample-depth",  default=16384,       help="Sampledepth (per channel).")
     parser.add_argument("--control-port",  default=5025,        help="Host bind Control port.")
     parser.add_argument("--control-only",  action="store_true", help="Only enable Control port.")
     parser.add_argument("--waveform-port", default=50101,       help="Host bind Waveform port.")
@@ -108,6 +146,9 @@ def main():
 
     server = SCPIServer(
         bind_ip       = args.bind_ip,
+        channels      = int(args.channels),
+        sample_rate   = int(args.sample_rate),
+        sample_depth  = int(args.sample_depth),
         control_port  = int(args.control_port),
         control_only  = args.control_only,
         waveform_port = int(args.waveform_port)
