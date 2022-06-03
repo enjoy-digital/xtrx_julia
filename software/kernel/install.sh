29e22e2163fad520f211f7881eb6c9000bcff4cf
Steve Kelly <kd2cca@gmail.com>
2022-06-03 16:31:23 +0000

    update installer with udev

--- software/kernel/install.sh
+++ software/kernel/install.sh
@@ -1,8 +1,9 @@
-#mkdir -p /lib/modules/$(uname -r)/litex
-cp litepcie.ko /lib/modules/$(uname -r)/litepcie.ko
-cp liteuart.ko /lib/modules/$(uname -r)/liteuart.ko
+mkdir -p /lib/modules/$(uname -r)/kernel/litex
+cp litepcie.ko /lib/modules/$(uname -r)/kernel/litex/litepcie.ko
+cp liteuart.ko /lib/modules/$(uname -r)/kernel/litex/liteuart.ko
 depmod -a
 modprobe litepcie
 modprobe liteuart
+cp 99-litepci.rules /etc/udev/rules.d/99-litepci.rules
 
-echo "Remember to add litepcie and liteuart to /etc/modules-load.d/modules.conf"
+echo "!!! Remember to add litepcie and liteuart to /etc/modules-load.d/modules.conf !!!!"
