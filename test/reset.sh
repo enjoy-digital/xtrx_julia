855addaa951e3b92431ec10fb666e52707d6c67c
Steve Kelly <kd2cca@gmail.com>
2022-06-13 17:09:33 +0000

    add test scripts

---
+++ test/reset.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+sudo bash -c 'echo "1" > /sys/bus/pci/devices/0000\:02\:00.0/reset'
---
+++ test/test_dma.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+export SOAPY_SDR_PLUGIN_PATH=$(pwd)/../software/soapysdr/build
+
+echo "Testing CPU DMA"
+julia --project=../software/scripts/ ../software/scripts/test_pattern.jl
+
+echo "Testing GPU DMA"
+julia --project=../software/scripts/ ../software/scripts/test_pattern_gpu.jl
