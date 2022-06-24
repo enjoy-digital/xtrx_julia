7bd89acf27cc602754385f3418c9e583a1eb03dd
Steve Kelly <kd2cca@gmail.com>
2022-06-24 18:11:12 +0000

    update settings test

--- software/scripts/test_settings.jl
+++ software/scripts/test_settings.jl
@@ -6,6 +6,7 @@ end
 
 using SoapySDR
 using Test
+using Unitful
 
 SoapySDR.register_log_handler()
 
@@ -21,31 +22,55 @@ for sensor in dev.sensors
     @info "Sensor Readout:  $(sensor.name) : $(dev[sensor])"
 end
 
+@info "Clock Settings..."
+@testset "Clock Settings" begin
+    
+end
+
 @testset "Channels" begin
 for dir in (dev.rx, dev.tx)
     for i in eachindex(dir)
         txrx = dir[i]
         @testset "Channel: $txrx" begin
-            @info "Testing Sample Rate Settings..."
+            @testset "Antenna Settings" begin
+            for antenna in txrx.antennas
+                txrx.antenna = antenna
+                @test txrx.antenna == antenna
+            end
+            end
+            @testset "Sample Rate Settings" begin
             for rate in SoapySDR.sample_rate_ranges(txrx)
                 txrx.sample_rate = first(rate)
                 @test txrx.sample_rate == first(rate)
                 txrx.sample_rate = last(rate)
                 @test txrx.sample_rate == last(rate)
             end
-            @info "Testing Bandwidth Settings..."
+            end
+            @testset "Bandwidth Settings" begin
             for rate in SoapySDR.bandwidth_ranges(txrx)
                 txrx.bandwidth = first(rate)
                 @test txrx.bandwidth == first(rate)
                 txrx.bandwidth = last(rate)
                 @test txrx.bandwidth == last(rate)
             end
-            @info "Testing Frequency Settings..."
+            end
+            @testset "Frequency Settings" begin
             for rate in SoapySDR.frequency_ranges(txrx)
+                first(rate) < 0u"Hz" && continue 
                 txrx.frequency = first(rate)
                 @test txrx.frequency == first(rate)
                 txrx.frequency = last(rate)
                 @test txrx.frequency == last(rate)
+            end
+            end
+            @testset "Gain Settings" begin
+            for gainelt in txrx.gain_elements
+                rng = SoapySDR.gainrange(txrx, gainelt)
+                txrx[gainelt] = first(rng)
+                @test txrx[gainelt] == first(rng)
+                txrx[gainelt] = last(rng)
+                @test txrx[gainelt] == last(rng)
+            end
             end
         end
     end
