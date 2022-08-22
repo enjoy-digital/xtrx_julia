5e636350af14fd14b1996a2735962faef829a9b2
Tim Besard <tim.besard@gmail.com>
2022-08-22 12:32:58 +0200

    Fix module insertion script and remove broken rebuild functionality.

--- software/litepcie-kernel-module/init.sh
+++ software/litepcie-kernel-module/init.sh
@@ -1,30 +1,23 @@
 #!/bin/sh
 # TODO: use udev instead
 
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 FOUND=$(lsmod | grep litepcie)
 if [ "$FOUND" != "" ] ; then
     echo "Module already installed"
     exit 0
 fi
 
-INS=$(sudo insmod litepcie.ko 2>&1)
+INS=$(sudo insmod ${DIR}/litepcie.ko 2>&1)
 if [ "$?" != "0" ] ; then
     ERR=$(echo $INS | sed -s "s/.*litepcie.ko: //")
     case $ERR in
     'Invalid module format')
-        set -e
-        echo "Kernel may have changed, try to rebuild module"
-        make -s clean
-        make -s
-        sudo insmod litepcie.ko
-        set +e
+        echo "Kernel may have changed, please rebuild the kernel modules"
         ;;
     'No such file or directory')
-        set -e
         echo "Module not compiled"
-        make -s
-        sudo insmod litepcie.ko
-        set +e
         ;;
     'Required key not available')
         echo "Can't insert kernel module, secure boot is probably enabled"
@@ -37,7 +30,7 @@ if [ "$?" != "0" ] ; then
     esac
 fi
 
-sudo insmod liteuart.ko
+sudo insmod ${DIR}/liteuart.ko
 
 for i in `seq 0 16` ; do
     sudo chmod 666 /dev/litepcie$i > /dev/null 2>&1
