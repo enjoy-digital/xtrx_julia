a1aff902b79ff2afad854954718e59e76ebdd254
Florent Kermarrec <florent@enjoy-digital.fr>
2022-09-01 14:47:43 +0200

    target: Add DNA.

--- fairwaves_xtrx.py
+++ fairwaves_xtrx.py
@@ -27,6 +27,7 @@ from litex.soc.cores.gpio import GPIOOut
 from litex.soc.cores.spi_flash import S7SPIFlash
 from litex.soc.cores.bitbang import I2CMaster
 from litex.soc.cores.xadc import XADC
+from litex.soc.cores.dna  import DNA
 
 from litepcie.phy.s7pciephy import S7PCIEPHY
 
@@ -69,6 +70,7 @@ class BaseSoC(SoCCore):
         "icap"        : 1,
         "flash"       : 2,
         "xadc"        : 3,
+        "dna"         : 4,
 
         # PCIe.
         "pcie_phy"    : 10,
@@ -128,6 +130,10 @@ class BaseSoC(SoCCore):
 
         # XADC -------------------------------------------------------------------------------------
         self.submodules.xadc = XADC()
+
+        # DNA --------------------------------------------------------------------------------------
+        self.submodules.dna = DNA()
+        self.dna.add_timing_constraints(platform, sys_clk_freq, self.crg.cd_sys.clk)
 
         # PCIe -------------------------------------------------------------------------------------
         self.submodules.pcie_phy = S7PCIEPHY(platform, platform.request(f"pcie_x2"),
