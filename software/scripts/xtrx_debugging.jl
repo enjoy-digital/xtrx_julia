c03e2c6ce020607f6fb4f3e8e402ae4bab9a9f00
Elliot Saba <staticfloat@gmail.com>
2022-09-06 17:43:10 +0000

    Add ability to toggle RxTSP registers from Julia

--- software/scripts/xtrx_debugging.jl
+++ software/scripts/xtrx_debugging.jl
@@ -73,3 +73,89 @@ function do_clock_scan(kwargs...)
     end
 end
 =#
+
+mutable struct RxTSPComponentEnables
+    dc_tracking_loop::Bool
+    CMIX::Bool
+    AGC::Bool
+    GFIR3::Bool
+    GFIR2::Bool
+    GFIR1::Bool
+    dc_corrector::Bool
+    gain_corrector::Bool
+    phase_corrector::Bool
+end
+
+RxTSPComponentEnables() = RxTSPComponentEnables(false, false, false, false, false, false, false, false, false)
+
+function deserialize(reg::UInt16)
+    x = RxTSPComponentEnables(
+        # DCLOOP_STOP
+        !get_bit(8, reg),
+        # CMIX_BYP
+        !get_bit(7, reg),
+        # AGC_BYP
+        !get_bit(6, reg),
+        # GFIR3_BYP
+        !get_bit(5, reg),
+        # GFIR2_BYP
+        !get_bit(4, reg),
+        # GFIR1_BYP
+        !get_bit(3, reg),
+        # DC_BYP
+        !get_bit(2, reg),
+        # GC_BYP
+        !get_bit(1, reg),
+        # PH_BYP
+        !get_bit(0, reg),
+    )
+    return x
+end
+
+function serialize(rtcp::RxTSPComponentEnables)
+    return UInt16(
+        set_bit(8, !rtcp.dc_tracking_loop) |
+        set_bit(7, !rtcp.CMIX) |
+        set_bit(6, !rtcp.AGC) |
+        set_bit(5, !rtcp.GFIR3) |
+        set_bit(4, !rtcp.GFIR2) |
+        set_bit(3, !rtcp.GFIR1) |
+        set_bit(2, !rtcp.dc_corrector) |
+        set_bit(1, !rtcp.gain_corrector) |
+        set_bit(0, !rtcp.phase_corrector)
+    )
+end
+
+mutable struct RxTSPConfig
+    enables::RxTSPComponentEnables
+end
+RxTSPConfig() = RxTSPConfig(RxTSPComponentEnables())
+
+function Base.show(io::IO, rx_tsp::RxTSPConfig)
+    println(io, "RxTSPConfig")
+    println(io, "  Enabled stages:")
+    for field in (:dc_corrector, :dc_tracking_loop, :gain_corrector, :phase_corrector, :CMIX, :AGC, :GFIR1, :GFIR2, :GFIR3)
+        if getfield(rx_tsp.enables, field)
+            println(io, "    - $(field)")
+        end
+    end
+end
+
+function Base.read!(dev::Device, rx_tsp::RxTSPConfig)
+    # We just always read from channel A
+    set_mac(dev, :A)
+    rx_tsp.enables = deserialize(read_lms_register(dev, 0x040C))
+    return rx_tsp
+end
+
+function Base.write(dev::Device, rx_tsp::RxTSPConfig)
+    for channel in (:A, :B)
+        # Read back the other configuration bits:
+        set_mac(dev, channel)
+        reg = read_lms_register(dev, 0x040C)
+
+        # Insert our new RxTSPConfig values
+        reg = (reg & 0xfe00) | serialize(rx_tsp.enables)
+        write_lms_register(dev, 0x040C, reg)
+    end
+end
