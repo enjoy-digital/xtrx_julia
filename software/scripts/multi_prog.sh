a000a7315d357499125b8081105bcef4ade9e1e8
Steve Kelly <kd2cca@gmail.com>
2022-09-06 15:26:45 +0000

    use parallel for programming since SPI flash is slow

--- software/scripts/multi_prog.sh
+++ software/scripts/multi_prog.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/env bash
 
-for i in `seq 0 8`;
-    do ../litepcie-user-library/litepcie_util -c $i flash_write ../../build/fairwaves_xtrx_platform/gateware/fairwaves_xtrx_platform.bin;
-done
+seq 0 8 | parallel ../litepcie-user-library/litepcie_util -c {} flash_write ../../build/fairwaves_xtrx_platform/gateware/fairwaves_xtrx_platform.bin
