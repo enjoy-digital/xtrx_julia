6c5d8de02c7ff59b1c908b9b30a32410f91bfb79
Steve Kelly <kd2cca@gmail.com>
2022-09-07 15:56:17 +0000

    cleanup LED location finding

--- software/scripts/toggle_led.jl
+++ software/scripts/toggle_led.jl
@@ -2,9 +2,15 @@ using SoapySDR
 
 
 for dev in Devices(driver="XTRX")
-    println("serial:", dev["serial"])
+    Device(dev) do d
+        SoapySDR.SoapySDRDevice_writeRegister(d, "LitePCI", 0xf0000000|0x4800, 0)
+    end
+end
+
+for dev in Devices(driver="XTRX")
     Device(dev) do d
         SoapySDR.SoapySDRDevice_writeRegister(d, "LitePCI", 0xf0000000|0x4800, 1)
+        println("serial:", dev["serial"])
         println("press enter to continue")
         readline()
         SoapySDR.SoapySDRDevice_writeRegister(d, "LitePCI", 0xf0000000|0x4800, 0)
