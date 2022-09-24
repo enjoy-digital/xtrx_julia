247374c730bec19d3c365498dbd5f34761546885
Steve Kelly <kd2cca@gmail.com>
2022-09-24 01:38:04 +0000

    serialize programming since parallel is risky

--- software/scripts/multi_prog.sh
+++ software/scripts/multi_prog.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/env bash
 
-seq 0 8 | parallel ../litepcie-user-library/litepcie_util -c {} flash_write ../../build/fairwaves_xtrx_platform/gateware/fairwaves_xtrx_platform.bin
+
+#seq 0 8 | parallel ../litepcie-user-library/litepcie_util -c {} flash_write ../../build/fairwaves_xtrx_platform/gateware/fairwaves_xtrx_platform.bin
+for i in `seq 0 8`;
+    do ../litepcie-user-library/litepcie_util -c $i flash_write ../../build/fairwaves_xtrx_platform/gateware/fairwaves_xtrx_platform.bin;
+done
