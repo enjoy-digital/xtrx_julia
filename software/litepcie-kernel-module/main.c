82e3fe4a4487a349282d5aa192c22ae75eebd65a
Steve Kelly <kd2cca@gmail.com>
2022-11-15 21:34:39 +0000

    set DMA_BIT_MASK to DMA_ADDR_WIDTH
    According to: https://www.kernel.org/doc/Documentation/DMA-API-HOWTO.txt
    This should match the address width supported by the gateware

--- software/litepcie-kernel-module/main.c
+++ software/litepcie-kernel-module/main.c
@@ -38,6 +38,7 @@
 #include "csr.h"
 #include "config.h"
 #include "flags.h"
+#include "soc.h"
 
 #define NV_DMA
 
@@ -1343,9 +1344,9 @@ static int litepcie_pci_probe(struct pci_dev *dev, const struct pci_device_id *i
 
 	pci_set_master(dev);
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0)
-	ret = pci_set_dma_mask(dev, DMA_BIT_MASK(32));
+	ret = pci_set_dma_mask(dev, DMA_BIT_MASK(DMA_ADDR_WIDTH));
 #else
-	ret = dma_set_mask(&dev->dev, DMA_BIT_MASK(32));
+	ret = dma_set_mask(&dev->dev, DMA_BIT_MASK(DMA_ADDR_WIDTH));
 #endif
 	if (ret) {
 		dev_err(&dev->dev, "Failed to set DMA mask\n");
